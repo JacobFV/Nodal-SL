3d2fbd0afea1c9b5c6566db706bb5bbe4a2bec2c
Jacob Valdez <jacobfv@msn.com>
2021-01-19 15:02:55 -0600

    moving to colab

---
+++ __main__.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+#cpus = tf.config.get_visible_devices('CPU')
+#tf.config.set_visible_devices(cpus)
+#tf.config.experimental.set_virtual_device_configuration(cpus[0], [
+#    tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+
+import nodal_sl
